1c0b30aeff18e1844b0318ab1f45be2f783ff0e4
Danielle Puga <danielle.puga@gmail.com>
2023-04-19 11:12:18 -0700

    business post

--- app.js
+++ app.js
@@ -9,14 +9,49 @@ const PORT = 3000;
 // add info to it. we can pre-populate with data to
 // test and error check.
 
-// test comment
+// structure:
+// app.METHOD(PATH, MIDDLEWARE);
+
+// test array with all fields. blanks are optional
+let businesses = [
+    { businessName: "Test123",
+      businessAddress: "123 Cherry Tree Lane",
+      businessCity: "Bend",
+      businessState: "Oregon",
+      businessZip: "55555",
+      businessPhone: "555-123-4567",
+      businessCategory: "Restaurant",
+      businessSubcategory: "Pizza",
+      businessWebste: "",
+      businessEmail: "",
+    }
+]
   
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.status(200);
     res.send("Welcome to root URL of Server");
 });
-  
-app.listen(PORT, (error) =>{
+
+app.get('/business', (req, res) => {
+    res.status(200);
+    res.send(businesses);
+});
+
+// use Postman to test this
+app.post('/business', (req, res) => {
+    let data = req.body;
+    res.send("Business information received: " + JSON.stringify(data));
+});
+
+
+// end catch all for paths we havent specified
+app.use('*', function (req, res) {
+    res.status(404).send({
+        err: "The requested resource doesn't exist"
+    });
+});
+
+app.listen(PORT, (error) => {
     if(!error)
         console.log("Server is Successfully Running, and App is listening on port "+ PORT)
     else 
